dd06ab43adcd30f61d6f01741e857ca26aeb190f
Benjamin Schwartz <bs4308@nyu.edu>
2022-08-18 15:03:47 -0400

    Start user associations

--- practice-for-ch-sql-associations-long-practice/app/models/user.rb
+++ practice-for-ch-sql-associations-long-practice/app/models/user.rb
@@ -1,2 +1,26 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
+  has_many(
+    :enrollments,
+    class_name: 'User',
+    foreign_key: :enrollment_id,
+    primary_key: :id
+  )
+
+  has_many(
+    :enrolled_courses,
+    class_name: 'Course',
+    foreign_key: :course_id,
+    primary_key: :id)
+
+  has_many :courses, through: :enrollments, source: :users
+
 end
